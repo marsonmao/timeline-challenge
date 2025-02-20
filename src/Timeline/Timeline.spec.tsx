e98e3229e1487893ab7323a0a3c4d5d4e35f4d6e
Marson Mao <marsonmao@gmail.com>
2025-02-20 21:57:13 +0800

    chore: refine test description

--- src/Timeline/Timeline.spec.tsx
+++ src/Timeline/Timeline.spec.tsx
@@ -2,17 +2,16 @@ import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Timeline } from "./Timeline";
 
-
-test("the value should reflect user input", async () => {
+test("the input field value should reflect user input", async () => {
   const { getByTestId } = render(<Timeline />);
-
   const currentTimeInput = getByTestId(
     "current-time-input"
   ) as HTMLInputElement;
 
+  // Initial render
   expect(currentTimeInput.value).toBe("0");
 
+  // After user input
   await userEvent.type(currentTimeInput, "20");
-
   expect(currentTimeInput.value).toBe("20");
 });
