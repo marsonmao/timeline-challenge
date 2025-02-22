b2ba452f83869e1ed1ff8834dfa45dedddc82f77
Marson Mao <marsonmao@gmail.com>
2025-02-22 22:59:48 +0800

    test: delete the example test

--- src/index.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-it.skip("renders welcome message", () => {
-  render(<App />);
-  expect(screen.getByText("Time")).toBeInTheDocument();
-});
