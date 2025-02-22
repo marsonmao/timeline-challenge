8ed0b8de69de96a20f54a6ca80ffd03837c99909
Marson Mao <marsonmao@gmail.com>
2025-02-22 12:35:21 +0800

    feat: implement arrow keys pressing and spinners clicking

--- src/Timeline/PlayControls.tsx
+++ src/Timeline/PlayControls.tsx
@@ -1,18 +1,129 @@
-import React, { memo, useCallback, useContext } from "react";
+import React, { memo, useCallback, useContext, useRef } from "react";
 import { TimelineContext } from "./TimelineContext";
 import { RenderTracker } from "./RenderTracker";
 
+const config = {
+  /**
+   * In unit of Milliseconds
+   */
+  timeStep: 10,
+  minEndTime: 100,
+  maxEndTime: 2000,
+};
+
+export function validateTime(time: number): number {
+  // TODO handle NaN
+  return Math.min(config.maxEndTime, Math.max(0, time));
+}
+
+/**
+    How this component work:
+
+    Entry (focus)
+    1. click on the input area
+    2. click on the spinners
+    3. tab
+
+    Change local time only
+    1. type in the input
+
+    Change global time directly
+    1. press enter
+    2. click outside (blur)
+    3. click on the spinners
+
+    Revert to global time
+    1. press escape
+ */
 export const PlayControls = () => {
   // TODO: implement time <= maxTime
 
-  const { time, setTime } = useContext(TimelineContext);
+  const [isEditingTime, setIsEditingTime] = React.useState(false);
+  const { time: globalTime, setTime: setGlobalTime } =
+    useContext(TimelineContext);
+  const [inputTime, setInputTime] = React.useState(globalTime);
+  const isSpinnerClicked = useRef(false);
+  const isArrowKeyDown = useRef(false);
+  const blurTriggerKey = useRef<string|null>(null);
 
-  const onTimeChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setTime(Number(e.target.value));
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const rawValue = Number(e.target.value);
+    const validatedValue = validateTime(rawValue);
+
+    if (isSpinnerClicked.current || isArrowKeyDown.current) {
+      setGlobalTime(validatedValue);
+
+      // TODO select all input text
+    }
+
+    setInputTime(validatedValue);
+  };
+
+  const onInputFocus = useCallback(() => {
+    setIsEditingTime(true);
+
+    // TODO select all input text
+  }, []);
+
+  const onInputBlur = () => {
+    if (blurTriggerKey.current === "Escape") {
+      setInputTime(globalTime);
+    } else if (
+      blurTriggerKey.current === "Enter" ||
+      blurTriggerKey.current === null
+    ) {
+      setGlobalTime(inputTime);
+    }
+
+    setIsEditingTime(false);
+    blurTriggerKey.current = null;
+  };
+
+  const onInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    /**
+     * If any key is pressed, it would break the spinner event chain, so we clear the flag.
+     */
+    isSpinnerClicked.current = false;
+
+    if (e.key === "Enter" || e.key === "Escape") {
+      blurTriggerKey.current = e.key;
+      e.currentTarget.blur();
+    } else if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+      isArrowKeyDown.current = true;
+    }
+  };
+
+  const onInputKeyUp = (_e: React.KeyboardEvent<HTMLInputElement>) => {
+    isArrowKeyDown.current = false;
+  };
+
+  const onInputMouseDown = useCallback(
+    (_e: React.MouseEvent<HTMLInputElement>) => {
+      /**
+       * The goal is to change the global time when the spinners are clicked.
+       * 
+       * However, we cannot know if they are clicked due to the limitation of browser implementation.
+       * The browser can only tell us that the input element was clicked, but it could be any pixel.
+       * 
+       * The workaround is to detect a chain of consecutive events: [mouse down, change].
+       * When a spinner was clicked, it would form this event chain.
+       * 
+       * So that in this handler, we firstly set the flag to true, 
+       * and then we check if the chain is formed in the other handlers.
+       */
+      isSpinnerClicked.current = true;
     },
-    [setTime]
+    []
   );
+
+  const onInputMouseUp = useCallback(
+    (_e: React.MouseEvent<HTMLInputElement>) => {
+      isSpinnerClicked.current = false;
+    },
+    []
+  );
+
+  const timeInputValue = isEditingTime ? inputTime : globalTime;
 
   return (
     <>
@@ -29,10 +140,16 @@ export const PlayControls = () => {
             type="number"
             data-testid="current-time-input"
             min={0}
-            max={2000}
-            step={10}
-            value={time}
-            onChange={onTimeChange}
+            max={config.maxEndTime}
+            step={config.timeStep}
+            value={timeInputValue}
+            onChange={onInputChange}
+            onFocus={onInputFocus}
+            onBlur={onInputBlur}
+            onKeyDown={onInputKeyDown}
+            onKeyUp={onInputKeyUp}
+            onMouseDown={onInputMouseDown}
+            onMouseUp={onInputMouseUp}
           />
         </fieldset>
         -
@@ -41,10 +158,10 @@ export const PlayControls = () => {
             className="bg-gray-700 px-1 rounded"
             type="number"
             data-testid="duration-input"
-            min={100}
-            max={2000}
-            step={10}
-            defaultValue={2000}
+            min={config.minEndTime}
+            max={config.maxEndTime}
+            step={config.timeStep}
+            defaultValue={config.maxEndTime}
           />
           Duration
         </fieldset>
