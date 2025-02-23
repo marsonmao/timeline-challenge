dbf0f82a5858a34d8303a0d54975ff8c821de555
Marson Mao <marsonmao@gmail.com>
2025-02-24 00:36:02 +0800

    feat: set segment width to duration time

--- src/Timeline/Segment.tsx
+++ src/Timeline/Segment.tsx
@@ -1,8 +1,16 @@
+import { useTime } from "./useTime";
+
 export const Segment = () => {
-  // TODO: resize based on time
+  const { durationTime } = useTime();
 
   return (
-    <div className="w-[2000px] py-2" data-testid="segment">
+    <div
+      className="py-2"
+      data-testid="segment"
+      style={{
+        width: `${durationTime}px`,
+      }}
+    >
       <div className="h-6 rounded-md bg-white/10"></div>
     </div>
   );
