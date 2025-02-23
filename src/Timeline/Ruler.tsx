cb911ae89b5338bf944c2a1803189c6d7a95db3b
Marson Mao <marsonmao@gmail.com>
2025-02-23 23:49:20 +0800

    feat: adjust ruler width to be duration time, also allow clicking and mousemoving to set current time

--- src/Timeline/Ruler.tsx
+++ src/Timeline/Ruler.tsx
@@ -1,8 +1,37 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import { RenderTracker } from "./RenderTracker";
+import { useTime } from "./useTime";
+import { validateNumber } from "./NumberInput";
 
 export const Ruler = () => {
   // TODO: implement mousedown and mousemove to update time and Playhead position
+  const { setCurrentTime, currentTimeConfig, durationTime } = useTime();
+
+  const handleClick = useCallback<
+    NonNullable<React.DOMAttributes<HTMLDivElement>["onClick"]>
+  >(
+    (e) => {
+      const { left } = e.currentTarget.getBoundingClientRect();
+      const clickX = e.clientX - left;
+      const millisecond = validateNumber(clickX, currentTimeConfig);
+      setCurrentTime(millisecond.result);
+    },
+    [currentTimeConfig]
+  );
+
+  const handleMouseMove = useCallback<
+    NonNullable<React.DOMAttributes<HTMLDivElement>["onMouseMove"]>
+  >(
+    (e) => {
+      if (e.buttons === 1) {
+        const { left } = e.currentTarget.getBoundingClientRect();
+        const mouseX = e.clientX - left;
+        const millisecond = validateNumber(mouseX, currentTimeConfig);
+        setCurrentTime(millisecond.result);
+      }
+    },
+    [currentTimeConfig]
+  );
 
   return (
     <>
@@ -14,8 +43,13 @@ export const Ruler = () => {
         data-testid="ruler"
       >
         <div
-          className="w-[2000px] h-6 rounded-md bg-white/25"
+          className="h-6 rounded-md bg-white/25"
           data-testid="ruler-bar"
+          style={{
+            width: `${durationTime}px`,
+          }}
+          onClick={handleClick}
+          onMouseMove={handleMouseMove}
         ></div>
       </div>
     </>
