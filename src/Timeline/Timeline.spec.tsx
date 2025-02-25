f1b66604e616ba10ae9a43fe3d4596a9d972b8d0
Marson Mao <marsonmao@gmail.com>
2025-02-25 15:23:54 +0800

    test: remove a redundant case and reuse a util

--- src/Timeline/Timeline.spec.tsx
+++ src/Timeline/Timeline.spec.tsx
@@ -1,20 +1,7 @@
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Timeline } from "./Timeline";
-
-test("the input field value should reflect user input", async () => {
-  const { getByTestId } = render(<Timeline />);
-  const currentTimeInput = getByTestId(
-    "current-time-input"
-  ) as HTMLInputElement;
-
-  // Initial render
-  expect(currentTimeInput.value).toBe("0");
-
-  // After user input
-  await userEvent.type(currentTimeInput, "20");
-  expect(currentTimeInput.value).toBe("20");
-});
+import { clickAndType } from "./test-util";
 
 test("the components subscribed to the time state should render accordingly, while the rest should not rerender", async () => {
   const { getByTestId } = render(<Timeline />);
@@ -34,8 +21,7 @@ test("the components subscribed to the time state should render accordingly, whi
   expect(trackListRenderTracker.textContent).toBe("1");
   expect(keyframeListRenderTracker.textContent).toBe("1");
 
-  await userEvent.click(currentTimeInput);
-  await userEvent.type(currentTimeInput, "20");
+  await clickAndType(currentTimeInput, "20");
   await userEvent.keyboard("{Enter}");
   expect(playControlsRenderTracker.textContent).toBe("2");
   expect(playheadRenderTracker.textContent).toBe("3");
