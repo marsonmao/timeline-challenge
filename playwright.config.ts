120bff65f5989a983bd1415852ee03e12e45ceef
Marson Mao <marsonmao@gmail.com>
2025-02-25 22:46:46 +0800

    build: setup playwright

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./e2e",
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  projects: [
+    {
+      name: "Chromium",
+      use: {
+        browserName: "chromium",
+      },
+    },
+    // NOTE: disabled for now because this is a homework assignment
+    // {
+    //   name: "Firefox",
+    //   use: {
+    //     browserName: "firefox",
+    //   },
+    // },
+    // {
+    //   name: "WebKit",
+    //   use: {
+    //     browserName: "webkit",
+    //   },
+    // },
+  ],
+  reporter: "html",
+});
